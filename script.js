21acdc7e2e01380e4778ec4109c43a8cb802fa5e
unknown <vanzandt.school.email@gmail.com>
2023-12-13 17:48:51 -0500

    Xpert assistant code added

--- script.js
+++ script.js
@@ -15,3 +15,47 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var length = parseInt(prompt("Enter the desired length of the password (between 8 and 128 characters):"));
+
+// Validate the user input
+if (isNaN(length) || length < 8 || length > 128) {
+  alert("Invalid password length. Please enter a number between 8 and 128.");
+} else {
+  // Prompt the user for character types to include
+  var includeLowercase = confirm("Include lowercase letters?");
+  var includeUppercase = confirm("Include uppercase letters?");
+  var includeNumbers = confirm("Include numbers?");
+  var includeSpecialChars = confirm("Include special characters?");
+
+  // Define character sets
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+  // Build the character set based on user selections
+  var charSet = "";
+  if (includeLowercase) {
+    charSet += lowercaseChars;
+  }
+  if (includeUppercase) {
+    charSet += uppercaseChars;
+  }
+  if (includeNumbers) {
+    charSet += numberChars;
+  }
+  if (includeSpecialChars) {
+    charSet += specialChars;
+  }
+
+  // Generate the password
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charSet.length);
+    password += charSet[randomIndex];
+  }
+
+  // Display the password to the user
+  alert("Your generated password is: " + password);
+}
