ad09b6894632f16dec0002860c4291827ef07de8
unknown <vanzandt.school.email@gmail.com>
2023-12-13 17:57:56 -0500

    testing generate password button

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
-// Assignment code here
+// // Assignment code here
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
-// Add event listener to generate button
+const generateBtn = document.getElementById(generate);
 generateBtn.addEventListener("click", writePassword);
 
-var length = parseInt(prompt("Enter the desired length of the password (between 8 and 128 characters):"));
+function writepassword() {
+    var length = parseInt(prompt("Enter the desired length of the password (between 8 and 128 characters):"));
+    if (isNaN(length) || length < 8 || length > 128) {
+      alert("Invalid password length. Please enter a number between 8 and 128.");
+    } else {
+}
+}
 
-// Validate the user input
+var length = parseInt(prompt("Enter the desired length of the password (between 8 and 128 characters):"));
 if (isNaN(length) || length < 8 || length > 128) {
   alert("Invalid password length. Please enter a number between 8 and 128.");
 } else {
-  // Prompt the user for character types to include
+ 
   var includeLowercase = confirm("Include lowercase letters?");
   var includeUppercase = confirm("Include uppercase letters?");
   var includeNumbers = confirm("Include numbers?");
   var includeSpecialChars = confirm("Include special characters?");
 
-  // Define character sets
+
   var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
   var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numberChars = "0123456789";
   var specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
-  // Build the character set based on user selections
   var charSet = "";
   if (includeLowercase) {
     charSet += lowercaseChars;
@@ -49,13 +54,11 @@ if (isNaN(length) || length < 8 || length > 128) {
     charSet += specialChars;
   }
 
-  // Generate the password
   var password = "";
   for (var i = 0; i < length; i++) {
     var randomIndex = Math.floor(Math.random() * charSet.length);
     password += charSet[randomIndex];
   }
 
-  // Display the password to the user
   alert("Your generated password is: " + password);
 }
